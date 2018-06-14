e165704a351b8c3be3be30c786f17a9dea3d973e
Jonathan <jonathanzejunhu@gmail.com>
2018-06-14 18:34:35 -0400

    add slider, still need to finish

--- js/creative.js
+++ js/creative.js
@@ -116,6 +116,21 @@
     }
   });
 
+  $('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+    }
+    else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+  });
+
   // $(document).scroll(function () {
   // 	  var $nav = $("#mainNavSec");
   // 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
